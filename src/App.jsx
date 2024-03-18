781673c13d16bb3dab28d7b681bb883c4ab495e5
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-18 17:10:19 +0530

    added constant informations:- created a js file defining all the constant informations like service names, partner companies, prices and so on

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/header"
 import Button from "./components/Button";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">hello world</h1>
-      <div className="pt-[4.75rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          something
-        </Button>
+      
+      <div className="pt-[4.75rem] overflow-hidden | lg:pt-[5.25rem]">
+        <Header />
         
       </div>
       <ButtonGradient />
