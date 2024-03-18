91e0357d047098dc23a6e139256e57f5116808bf
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-18 20:32:41 +0530

    created navbar(responsive):- add a header component inside the components directory and build the navbar and included it inside the app.jsx file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,28 +1,58 @@
+// import ButtonSvg from "../assets/svg/ButtonSvg";
+
+// const Button = ({ className, href, onClick, children, px, white }) => {
+//   const classes= `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white? 'text-n-8':'text-n-1'} ${className || ''}`; //classes for the button
+//   const spanClasses = 'relative z-10';   //classes for the text inside a button
+  
+  
+//   const renderButton = () => (     //Note:- here we are using parenthesis, not curly braces because we are using an implicit return
+//     //button element
+//     <button className={classes} onClick={onClick} >  
+//       <span className={spanClasses}>{children}</span> 
+//       {ButtonSvg(white)}
+//     </button>
+//   );
+
+//   const renderLink=()=>(          
+//     <a href={href} className={classes}>
+//       <span className={spanClasses}>{children}</span>
+//       {ButtonSvg(white)}
+//     </a>
+//   )
+
+//   return href             //if href exists inside the Button component in App.jsx
+//     ? renderLink()        //it will return the renderLink function
+//     : renderButton();     //else, it will return the renderButton function
+// };
+
+// export default Button;
+
+
+
+
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes= `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white? 'text-n-8':'text-n-1'} ${className || ''}`; //classes for the button
-  const spanClasses = 'relative z-10';   //classes for the text inside a button
-  
-  
-  const renderButton = () => (     //Note:- here we are using parenthesis, not curly braces because we are using an implicit return
-    //button element
-    <button className={classes} >  
-      <span className={spanClasses}>{children}</span> 
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses = "relative z-10";
+
+  const renderButton = () => (
+    <button className={classes} onClick={onClick}>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
 
-  const renderLink=()=>(          
+  const renderLink = () => (
     <a href={href} className={classes}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
-  )
+  );
 
-  return href             //if href exists inside the Button component in App.jsx
-    ? renderLink()        //it will return the renderLink function
-    : renderButton();     //else, it will return the renderButton function
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
