1533cd6ae4e1c4fea59a6f70370b451bf785bc95
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-19 18:12:36 +0530

    started hero section:added the tagline and descriptionin the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/header"
+import Hero from "./components/Hero"
 import Button from "./components/Button";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       
       <div className="pt-[4.75rem] overflow-hidden | lg:pt-[5.25rem]">
         <Header />
+        <Hero/>
         
       </div>
       <ButtonGradient />
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,3 +1,6 @@
+//This component is a skeleton to wrap all sections of the webpage
+
+
 import SectionSvg from '../assets/svg/SectionSvg';           //importing the cross svg for using with the separation horizontal line after each section
 
 const Section = ({ className, id, crosses, crossesOffset, customPaddings, children }) => {
@@ -11,7 +14,7 @@ const Section = ({ className, id, crosses, crossesOffset, customPaddings, childr
                    {/* -------------left and right vertical lines in each section--------- */}
 
       <div className='hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none | md:block | lg:left-7.5 | xl:left-10'/>
-      <div className='hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none | md:block | lg:right-7.5 | xl:right-10'/>
+      <div className='hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none | md:block | lg:right-7.5 | xl:right-10'/>
        
                                     {/* -------------cross svg------------ */}
 
