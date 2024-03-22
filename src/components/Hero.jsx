36c971587285863bcca11ad9da6d21e2312513d0
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-22 07:06:53 +0530

    web performance:- removed the lazy loading from the FCP images of the webpage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
     <Section id="hero" className="pt-[12rem] -mt-[5.25rem]" crosses crossesOffset="lg:translate-y-[5.25rem]" customPaddings >
         <div className="container relative" ref={parallaxRef}>
             <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] | md:mb-20 | lg:mb-[6rem]" >
-                <h1 className="h1 mb-6">Explore the Possibilities 	&nbsp;of AI&nbsp;Chatting with <span className="inline-block relative">Kairocraft{" "} <img src={curve} className="absolute top-full left-0 w-full | xl:-mt-2" width={624} height={28} alt="curve" loading="lazy"/></span></h1>
+                <h1 className="h1 mb-6">Explore the Possibilities 	&nbsp;of AI&nbsp;Chatting with <span className="inline-block relative">Kairocraft{" "} <img src={curve} className="absolute top-full left-0 w-full | xl:-mt-2" width={624} height={28} alt="curve" /></span></h1>
                 <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 | lg:mb-8">Unleash the power of AI within Kairocraft. Upgrade your productivity with Kairocraft, the open AI chat app.</p>
                 <Button href="#pricing" white>Get started</Button>
             </div>
@@ -25,7 +25,7 @@ const Hero = () => {
                     <div className="relative bg-n-8 rounded-[1rem]">
                         <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]"/>
                         <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden | md:aspect-[688/490] | lg:aspect-[1024/490]">
-                            <img src={robot} alt="AI" className="w-full scale-[1.7] translate-y-[8%] | md:scale-[1] md:-translate-y-[10%] | lg:-translate-y-[23%]" width={1024} height={490} loading="lazy"/>
+                            <img src={robot} alt="AI" className="w-full scale-[1.7] translate-y-[8%] | md:scale-[1] md:-translate-y-[10%] | lg:-translate-y-[23%]" width={1024} height={490} />
                             
                             
                             <Generating className="absolute left-4 right-4 bottom-5 | md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2"/>
@@ -34,7 +34,7 @@ const Hero = () => {
                                 <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl | xl:flex">
                                     {heroIcons.map((icon,index)=>(
                                         <li className="p-5" key={index}>
-                                            <img src={icon} width={24} height={25}  alt={icon } loading="lazy"/>
+                                            <img src={icon} width={24} height={25}  alt={icon } />
                                         </li>
                                     ))}
                                 </ul>
@@ -48,7 +48,7 @@ const Hero = () => {
                     <Gradient/>
                 </div>
                 <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 | md:-top-[46%] md:w-[138%] | lg:-top-[104%]">
-                    <img src={heroBackground} className="w-full" alt="hero" width={1440} height={1800} loading="lazy"/>
+                    <img src={heroBackground} className="w-full" alt="hero" width={1440} height={1800} />
                 </div>
                 <BackgroundCircles/>
             </div>
